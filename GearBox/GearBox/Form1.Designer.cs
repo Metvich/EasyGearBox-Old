6b6676730f7dad179bfad8ff1a5b72e200dec572
Metvich <111798776+Metvich@users.noreply.github.com>
2022-08-23 01:03:57 +0500

    Add files via upload

---
+++ GearBox/GearBox/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace GearBox
+{
+    partial class EasyGearBox
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EasyGearBox));
+            this.button0 = new System.Windows.Forms.Button();
+            this.textBoxOn = new System.Windows.Forms.TextBox();
+            this.textBoxOff = new System.Windows.Forms.TextBox();
+            this.buttonCount = new System.Windows.Forms.Button();
+            this.fLPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.fLPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBoxGearBox = new System.Windows.Forms.TextBox();
+            this.textBoxRPS = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(206, 12);
+            this.button0.Margin = new System.Windows.Forms.Padding(0);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(20, 20);
+            this.button0.TabIndex = 0;
+            this.button0.Text = "+";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // textBoxOn
+            // 
+            this.textBoxOn.Enabled = false;
+            this.textBoxOn.Location = new System.Drawing.Point(54, 12);
+            this.textBoxOn.Name = "textBoxOn";
+            this.textBoxOn.Size = new System.Drawing.Size(70, 20);
+            this.textBoxOn.TabIndex = 10;
+            this.textBoxOn.Text = "On";
+            this.textBoxOn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBoxOff
+            // 
+            this.textBoxOff.Enabled = false;
+            this.textBoxOff.Location = new System.Drawing.Point(130, 12);
+            this.textBoxOff.Name = "textBoxOff";
+            this.textBoxOff.Size = new System.Drawing.Size(70, 20);
+            this.textBoxOff.TabIndex = 11;
+            this.textBoxOff.Text = "Off";
+            this.textBoxOff.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // buttonCount
+            // 
+            this.buttonCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCount.Location = new System.Drawing.Point(12, 266);
+            this.buttonCount.Name = "buttonCount";
+            this.buttonCount.Size = new System.Drawing.Size(214, 46);
+            this.buttonCount.TabIndex = 2;
+            this.buttonCount.Text = "Calculate";
+            this.buttonCount.UseVisualStyleBackColor = true;
+            this.buttonCount.Click += new System.EventHandler(this.buttonCount_Click);
+            // 
+            // fLPanel
+            // 
+            this.fLPanel.AutoScroll = true;
+            this.fLPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fLPanel.Location = new System.Drawing.Point(11, 38);
+            this.fLPanel.Name = "fLPanel";
+            this.fLPanel.Size = new System.Drawing.Size(216, 222);
+            this.fLPanel.TabIndex = 12;
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.DimGray;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(36, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "0-100";
+            this.toolTip1.SetToolTip(this.textBox1, "Minimum RPS difference between near gears in %");
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(232, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 46);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Copy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // fLPanel1
+            // 
+            this.fLPanel1.AutoScroll = true;
+            this.fLPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fLPanel1.Location = new System.Drawing.Point(231, 38);
+            this.fLPanel1.Name = "fLPanel1";
+            this.fLPanel1.Size = new System.Drawing.Size(180, 222);
+            this.fLPanel1.TabIndex = 13;
+            // 
+            // textBoxGearBox
+            // 
+            this.textBoxGearBox.Enabled = false;
+            this.textBoxGearBox.Location = new System.Drawing.Point(232, 12);
+            this.textBoxGearBox.Name = "textBoxGearBox";
+            this.textBoxGearBox.Size = new System.Drawing.Size(72, 20);
+            this.textBoxGearBox.TabIndex = 36;
+            this.textBoxGearBox.Text = "Gears";
+            this.textBoxGearBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBoxRPS
+            // 
+            this.textBoxRPS.Enabled = false;
+            this.textBoxRPS.Location = new System.Drawing.Point(310, 12);
+            this.textBoxRPS.Name = "textBoxRPS";
+            this.textBoxRPS.Size = new System.Drawing.Size(57, 20);
+            this.textBoxRPS.TabIndex = 37;
+            this.textBoxRPS.Text = "RPS";
+            this.textBoxRPS.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(373, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(37, 20);
+            this.textBox2.TabIndex = 38;
+            this.textBox2.Text = "0";
+            this.toolTip1.SetToolTip(this.textBox2, "Number of gears");
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 5000;
+            this.toolTip1.InitialDelay = 500;
+            this.toolTip1.ReshowDelay = 100;
+            // 
+            // EasyGearBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 324);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBoxRPS);
+            this.Controls.Add(this.textBoxGearBox);
+            this.Controls.Add(this.fLPanel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.fLPanel);
+            this.Controls.Add(this.textBoxOff);
+            this.Controls.Add(this.textBoxOn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonCount);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "EasyGearBox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EasyGearBox";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.TextBox textBoxOn;
+        private System.Windows.Forms.TextBox textBoxOff;
+        private System.Windows.Forms.Button buttonCount;
+        private System.Windows.Forms.FlowLayoutPanel fLPanel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel fLPanel1;
+        private System.Windows.Forms.TextBox textBoxGearBox;
+        private System.Windows.Forms.TextBox textBoxRPS;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
---
+++ GearBox/GearBox/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GearBox
+{
+    public partial class EasyGearBox : Form
+    {
+        float[] GearRPS;
+        bool[,] GearB;
+        int NumGB = 0, n = 0, nGear = 0;
+        float MinDif;
+        string str = "";
+        public EasyGearBox()
+        {
+            InitializeComponent();
+        }
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if ((sender as TextBox).ForeColor == Color.DimGray)
+            {
+                (sender as TextBox).Text = null;
+                (sender as TextBox).ForeColor = Color.Black;
+            }
+        }
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if ((sender as TextBox).Text == "")
+            {
+                (sender as TextBox).ForeColor = Color.DimGray;
+                (sender as TextBox).Text = "0-100";
+            }
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < 48 || e.KeyChar > 57) && e.KeyChar != 8 && e.KeyChar != 127) e.Handled = true;
+            else if (e.KeyChar != 8)
+            {
+                str = textBox1.Text;
+                if (e.KeyChar > 47 && e.KeyChar < 58)
+                {
+                    if (textBox1.SelectionStart == str.Length) str += e.KeyChar;
+                    else str = str.Replace(str.Substring(textBox1.SelectionStart, textBox1.SelectionLength), e.KeyChar.ToString());
+                    if (float.Parse(str) > 100) e.Handled = true;
+                }
+                str = null;
+            }
+        }
+        private void button0_Click(object sender, EventArgs e)
+        {
+            if (NumGB < 11)
+            {
+                TextBox tb = new TextBox();
+                NumericUpDown[] nUD = new NumericUpDown[4];
+                Button b = new Button();
+                tb.Name = "tB" + NumGB.ToString();
+                tb.Text = "GB" + NumGB.ToString();
+                tb.Enabled = false;
+                tb.Size = new Size(36, 20);
+                tb.Margin = new Padding(0, 0, 3, 0);
+                fLPanel.Controls.Add(tb);
+                for (int i = 0; i < 4; i++)
+                {
+                    nUD[i] = new NumericUpDown();
+                    nUD[i].Value = 1;
+                    nUD[i].Margin = new Padding(3, 0, 3, 0);
+                    nUD[i].Minimum = 1;
+                    nUD[i].Maximum = 9;
+                    nUD[i].Size = new Size(32, 20);
+                    nUD[i].Name = "nUD" + NumGB.ToString() + i.ToString();
+                    fLPanel.Controls.Add(nUD[i]);
+                }
+                b.Text = "-";
+                b.Name = "b" + NumGB.ToString();
+                b.Margin = new Padding(3, 0, 0, 0);
+                b.Size = new Size(20, 20);
+                b.Click += new System.EventHandler(this.buttonMin_Click);
+                fLPanel.Controls.Add(b);
+                NumGB++;
+            }
+        }
+        private void buttonMin_Click(object sender, EventArgs e)
+        {
+            NumGB--;
+            for (int i = int.Parse((sender as Button).Name.Substring(1)); i < NumGB; i++) for (int j = 0; j < 4; j++) (fLPanel.Controls["nUD" + i.ToString() + j.ToString()] as NumericUpDown).Value = (fLPanel.Controls["nUD" + (i + 1).ToString() + j.ToString()] as NumericUpDown).Value;
+            fLPanel.Controls.Remove(fLPanel.Controls["tB" + NumGB.ToString()]);
+            fLPanel.Controls.Remove(fLPanel.Controls["b" + NumGB.ToString()]);
+            for (int i = 0; i < 4; i++) fLPanel.Controls.Remove(fLPanel.Controls["nUD" + NumGB.ToString() + i.ToString()]);
+        }
+        void buttonMin_Click1(object sender, EventArgs e)
+        {
+            int k = int.Parse((sender as Button).Name.Substring(2));
+            str = (fLPanel1.Controls["tBr" + k.ToString()] as TextBox).Text.Replace('.', ',');
+            MinDif = float.Parse(str);
+            for (int i = 0; i < n; i++) if (MinDif == GearRPS[i]) GearRPS[i] = -1;
+            fLPanel1.Controls.Remove(fLPanel1.Controls["tBg" + k.ToString()]);
+            fLPanel1.Controls.Remove(fLPanel1.Controls["tBr" + k.ToString()]);
+            fLPanel1.Controls.Remove(fLPanel1.Controls["bg" + k.ToString()]);
+            for (int i = k + 1; i < nGear; i++)
+            {
+                (fLPanel1.Controls["tBg" + i.ToString()] as TextBox).Name = "tBg" + (i - 1).ToString();
+                (fLPanel1.Controls["tBr" + i.ToString()] as TextBox).Name = "tBr" + (i - 1).ToString();
+                (fLPanel1.Controls["bg" + i.ToString()] as Button).Name = "bg" + (i - 1).ToString();
+            }
+            nGear--;
+            textBox2.Text = nGear.ToString();
+            str = null;
+        }
+        private void buttonCount_Click(object sender, EventArgs e)
+        {
+            if (NumGB == 0) button0.Focus();
+            else
+            {
+                if (textBox1.ForeColor == Color.DimGray) MinDif = 0;
+                else MinDif = float.Parse(textBox1.Text);
+                n = (int)Math.Pow(2, NumGB);
+                GearRPS = new float[n];
+                GearB = new bool[n, NumGB];
+                for (int i = 0; i < n; i++)
+                {
+                    GearRPS[i] = 10000;
+                    int ResGearBox = i;
+                    for (int j = 0; j < NumGB; j++, ResGearBox /= 2)
+                    {
+                        GearB[i, j] = (ResGearBox % 2) == 1;
+                        if (GearB[i, j])
+                        {
+                            GearRPS[i] *= (float)(fLPanel.Controls["nUD" + j.ToString() + "1"] as NumericUpDown).Value;
+                            GearRPS[i] /= (float)(fLPanel.Controls["nUD" + j.ToString() + "0"] as NumericUpDown).Value;
+                        }
+                        else
+                        {
+                            GearRPS[i] *= (float)(fLPanel.Controls["nUD" + j.ToString() + "3"] as NumericUpDown).Value;
+                            GearRPS[i] /= (float)(fLPanel.Controls["nUD" + j.ToString() + "2"] as NumericUpDown).Value;
+                        }
+                    }
+                }
+                for (int i = 0; i < n; i++) if (GearRPS[i] != -1) GearRPS[i] = (float)(Math.Round(GearRPS[i] * 10) / 1000);
+                for (int i = 0; i < n; i++)
+                {
+                    if (GearRPS[i] == -1) continue;
+                    nGear = i;
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        if (GearRPS[nGear] > GearRPS[j] && GearRPS[j] != -1) nGear = j;
+                        else if (GearRPS[nGear] == GearRPS[j] && GearRPS[j] != -1) GearRPS[j] = -1;
+                    }
+                    if (nGear == i) continue;
+                    (GearRPS[i], GearRPS[nGear]) = (GearRPS[nGear], GearRPS[i]);
+                    for (int k = 0; k < NumGB; k++) (GearB[i, k], GearB[nGear, k]) = (GearB[nGear, k], GearB[i, k]);
+                }
+                nGear = 0;
+                if (MinDif != 0)
+                {
+                    for (int i = 0; i < (n - 1); i++)
+                    {
+                        if (GearRPS[i] == -1) continue;
+                        for (int j = i + 1; j < n; j++)
+                        {
+                            if (GearRPS[j] == -1) continue;
+                            if ((100 * (1 - (GearRPS[i] / GearRPS[j]))) < MinDif)
+                            {
+                                GearRPS[j] = -1;
+                                i = -1;
+                            }
+                            break;
+                        }
+                    }
+                }
+                fLPanel1.Controls.Clear();
+                for (int i = 0; i < n; i++)
+                {
+                    if (GearRPS[i] != -1)
+                    {
+                        str = "";
+                        for (int j = 0; j < NumGB; j++) str += GearB[i, j] ? "1" : "0";
+                        TextBox tBg = new TextBox();
+                        tBg.Text = str;
+                        tBg.Name = "tBg" + nGear.ToString();
+                        tBg.Size = new Size(72, 20);
+                        tBg.Margin = new Padding(0, 0, 3, 0);
+                        tBg.Enabled = false;
+                        fLPanel1.Controls.Add(tBg);
+                        str = GearRPS[i].ToString().Replace(',', '.');
+                        if (str.IndexOf('.') == -1) str += ".00";
+                        else if (str.Substring(str.IndexOf('.')).Length == 2) str += "0";
+                        tBg = new TextBox();
+                        tBg.Text = str;
+                        tBg.Name = "tBr" + nGear.ToString();
+                        tBg.Size = new Size(57, 20);
+                        tBg.Margin = new Padding(3, 0, 3, 0);
+                        tBg.Enabled = false;
+                        tBg.TextAlign = HorizontalAlignment.Center;
+                        fLPanel1.Controls.Add(tBg);
+                        Button bg = new Button();
+                        bg.Text = "-";
+                        bg.Margin = new Padding(3, 0, 0, 0);
+                        bg.Size = new Size(20, 20);
+                        bg.Name = "bg" + nGear.ToString();
+                        bg.Click += new System.EventHandler(this.buttonMin_Click1);
+                        fLPanel1.Controls.Add(bg);
+                        nGear++;
+                    }
+                }
+                textBox2.Text = nGear.ToString();
+                str = null;
+            }
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            str = "{";
+            for (int i = 0; i < NumGB; i++)
+            {
+                str += "{";
+                for (int j = 0, k = 0; j < n; j++) if (GearRPS[j] != -1)
+                    {
+                        str += (GearB[j, i] ? "1" : "0") + ((k == (nGear - 1)) ? "" : ",");
+                        k++;
+                    }
+                str += "}" + (i == (NumGB - 1) ? "}" : ",\n");
+            }
+            Clipboard.SetDataObject(str);
+            str = null;
+        }
+    }
+}
